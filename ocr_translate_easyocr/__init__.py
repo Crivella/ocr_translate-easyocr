4c36c9d597bd355840fe190176e228e908b6b507
Crivella <davidecrivella@yahoo.it>
2023-09-28 18:03:33 +0200

    Fix entrypoint

--- ocr_translate_easyocr/__init__.py
+++ ocr_translate_easyocr/__init__.py
@@ -18,7 +18,7 @@
 ###################################################################################
 """Plugins to enable usage of Easyocr in ocr_translate"""
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 easyocr_box_model_data = {
     # Name of the model
