71a7f4dba8df60628b9e9368fe27a636f062c9d1
Crivella <davidecrivella@yahoo.it>
2023-12-17 07:44:30 +0100

    Added advanced options description for ocr_translate v0.5

--- ocr_translate_easyocr/__init__.py
+++ ocr_translate_easyocr/__init__.py
@@ -18,7 +18,7 @@
 ###################################################################################
 """Plugins to enable usage of Easyocr in ocr_translate"""
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 easyocr_box_model_data = {
     'name': 'easyocr',
